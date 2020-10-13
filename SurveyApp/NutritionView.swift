812078007fdc23d37cd9dcff1c6e4e6439ebac01
aviswanath4 <aviswanath4@gmail.com>
2020-10-13 01:45:42 -0400

    added json of mini nutritional exam quesion

--- SurveyApp/NutritionView.swift
+++ SurveyApp/NutritionView.swift
@@ -14,7 +14,7 @@ struct Question: Decodable, Identifiable {
     let answers: [Answers]
 
     struct Answers: Decodable{
-        let id: String
+        let id: Int
         let score: Int
         let text : String
     }
@@ -24,36 +24,48 @@ struct Question: Decodable, Identifiable {
 struct NutritionView: View {
     var fetcher = Bundle.main.decode("MiniNutritional.json")
     @State var scores: Array<Int> = Array(repeating: 0, count: 100)
-
+    @State var isPressed: Array<Array<Bool>> = Array(repeating: Array(repeating: false, count: 10), count: 10)
+    
+    
 
     var body: some View {
-        List(fetcher) { question in
-                VStack (alignment: .leading){
-                    Text(question.text)
-                    .padding(5)
-                    ForEach(question.answers, id: \.id) {result in
-                        Button(action: {
-                            if(result.score != self.scores[question.id]){
-                                self.scores[question.id] = result.score
-                                }
-                        }, label: {
-                        Text("\(result.text)")})
-                        .contentShape(Rectangle())
-                        .buttonStyle(BorderlessButtonStyle())
+        //List(fetcher) { question in
+        //ForEach(fetcher.question, id: \.id) {result in
+        List{
+            VStack (alignment: .leading){
+                ForEach(fetcher) {question in
+                Text(question.text)
+                    .padding(10)
+                ForEach(question.answers, id: \.id) {result in
+                    Button(action: {
+                        if(result.score == 0 || result.score != self.scores[question.id]){
+                            self.isPressed[question.id][self.scores[question.id]] = false
+                            self.scores[question.id] = result.score
+                            self.isPressed[question.id][result.id] = true
+                            }
+                    }, label: {
+                    Text("\(result.text)")
                         .frame(maxWidth: .infinity, alignment: .center)
                         .multilineTextAlignment(.center)
                         .padding(10)
-                        .background(Color.blue)
                         .foregroundColor(Color.white)
-                        .cornerRadius(10)
+                        .background(self.isPressed[question.id][result.id] ? Color.blue : Color.gray)
+                    }
 
-                }
+                    )
+                    .cornerRadius(10)
+                    .buttonStyle(BorderlessButtonStyle())
+                    }
                 .padding(5)
-
+                }
+           // .multilineTextAlignment(.center)
             }
-            Text("\(self.scores.reduce(0, +))")
-
+            NavigationLink(destination: NutritionResults)
+//                    {Text("\(self.scores.reduce(0, +))")
+//                    }
         }
+        .navigationBarTitle("Nutrition View")
+        .padding()
     }
 }
 
