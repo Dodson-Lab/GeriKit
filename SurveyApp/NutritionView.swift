4b5503fad6403b8026944a4e4021e0b85c2bf612
aviswanath4 <aviswanath4@gmail.com>
2020-10-13 02:15:23 -0400

    Refactored code that reads in JSON and populates questions and buttons

--- SurveyApp/NutritionView.swift
+++ SurveyApp/NutritionView.swift
@@ -8,66 +8,24 @@
 
 import SwiftUI
 
-struct Question: Decodable, Identifiable {
-    let id: Int
-    let text: String
-    let answers: [Answers]
-
-    struct Answers: Decodable{
-        let id: Int
-        let score: Int
-        let text : String
-    }
-}
-
-
 struct NutritionView: View {
-    var fetcher = Bundle.main.decode("MiniNutritional.json")
-    @State var scores: Array<Int> = Array(repeating: 0, count: 100)
+    @State var MNA = Bundle.main.decode("MiniNutritional.json")
+    @State var MNAScore: Array<Int> = Array(repeating: 0, count: 100)
     @State var isPressed: Array<Array<Bool>> = Array(repeating: Array(repeating: false, count: 10), count: 10)
     
-    
-
     var body: some View {
-        //List(fetcher) { question in
-        //ForEach(fetcher.question, id: \.id) {result in
         List{
-            VStack (alignment: .leading){
-                ForEach(fetcher) {question in
-                Text(question.text)
-                    .padding(10)
-                ForEach(question.answers, id: \.id) {result in
-                    Button(action: {
-                        if(result.score == 0 || result.score != self.scores[question.id]){
-                            self.isPressed[question.id][self.scores[question.id]] = false
-                            self.scores[question.id] = result.score
-                            self.isPressed[question.id][result.id] = true
-                            }
-                    }, label: {
-                    Text("\(result.text)")
-                        .frame(maxWidth: .infinity, alignment: .center)
-                        .multilineTextAlignment(.center)
-                        .padding(10)
-                        .foregroundColor(Color.white)
-                        .background(self.isPressed[question.id][result.id] ? Color.blue : Color.gray)
-                    }
-
-                    )
-                    .cornerRadius(10)
-                    .buttonStyle(BorderlessButtonStyle())
-                    }
-                .padding(5)
+            QuestionView(fetcher: $MNA, scores: $MNAScore)
+            Section {
+                NavigationLink(destination: NutritionResults()){
+                    Text("Click here for your MNA®️ Results \(MNAScore.reduce(0, +))").bold()
                 }
-           // .multilineTextAlignment(.center)
             }
-            NavigationLink(destination: NutritionResults)
-//                    {Text("\(self.scores.reduce(0, +))")
-//                    }
         }
-        .navigationBarTitle("Nutrition View")
-        .padding()
+        .navigationBarTitle("Mini Nutrition Assessment")
     }
 }
+
 
 
 
