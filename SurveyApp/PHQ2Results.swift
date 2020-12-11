a3a339ec86786ea1c312cdb20dcce56c887fe5f7
Ambika Viswanathan <aviswanath4@gmail.com>
2020-12-11 01:40:19 -0500

    changed phq2 to include json file and question format

--- SurveyApp/PHQ2Results.swift
+++ SurveyApp/PHQ2Results.swift
@@ -12,11 +12,11 @@ struct PHQ2Results: View {
     @Binding var numberPHQ2Yes: Int
     
     func PHQ2Results(number: Int)-> String{
-        if number > 0{
-            return("Given the patient answered yes to one or more of these questions, the patient should be further evaluated with the PHQ-9:")
+        if number > 2{
+            return("they should be further evaluated with the PHQ-9:")
         }
         else {
-            return("The patient did not meet screening criteria for depression.")
+            return("they did not meet screening criteria for depression.")
         }
     }
     
@@ -27,11 +27,11 @@ struct PHQ2Results: View {
                 
             Spacer().frame(height: 30)
              
-            Text("\(PHQ2Results(number:numberPHQ2Yes))").bold().font(.system(size: 30)).multilineTextAlignment(.center)
+            Text("The patient's score of \(numberPHQ2Yes) suggests \(PHQ2Results(number:numberPHQ2Yes))").bold().font(.system(size: 30)).multilineTextAlignment(.center)
            
             Spacer().frame(height: 30)
 
-            if(self.numberPHQ2Yes > 0) {
+            if(self.numberPHQ2Yes > 2) {
                 NavigationLink(destination: PHQ9View()) {Text("Click here for PHQ9")}
                     .padding(10)
                     .background(Color.blue)
