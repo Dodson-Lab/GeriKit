27c3358ea3454a6e21aaec4b6e0c63efb7af2825
gabbyflabby <gabby1031@icloud.com>
2023-02-02 15:03:21 -0500

    feat: add frailty across the board

--- SurveyApp/ContentView.swift
+++ SurveyApp/ContentView.swift
@@ -15,118 +15,126 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             List{
-                NavigationLink(destination: CognitionChoice())
-                {HStack{
-                    Image("brain")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width:80)
-                        .padding()
-                    Text("Cognition")
+                Group {
+                    NavigationLink(destination: CognitionChoice())
+                    {HStack{
+                        Image("brain")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width:80)
+                            .padding()
+                        Text("Cognition")
                     }}
-                NavigationLink(destination: PHQ2View())
-                {HStack{
-                    Image("emotion")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width:80)
-                        .padding()
-                    Text("Depression")
+                    NavigationLink(destination: PHQ2View())
+                    {HStack{
+                        Image("emotion")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width:80)
+                            .padding()
+                        Text("Depression")
                     }}
-                NavigationLink(destination: ADLView())
-                  {HStack{
-                      Image("elderly")
-                          .resizable()
-                          .aspectRatio(contentMode: .fit)
-                          .frame(width:80)
-                          .padding()
-                      Text("Function")
-                      }}
-                NavigationLink(destination: ChairView())
-                  {HStack{
-                      Image("chair")
-                          .resizable()
-                          .aspectRatio(contentMode: .fit)
-                          .frame(width:80)
-                          .padding()
-                      Text("Strength")
-                      }}
-                NavigationLink(destination: FallView())
-                  {HStack{
-                      Image("fall")
-                          .resizable()
-                          .aspectRatio(contentMode: .fit)
-                          .frame(width:80)
-                          .padding()
-                      Text("Falls")
-                      }}
-                NavigationLink(destination: NutritionView())
-                  {HStack{
-                      Image("kitchen")
-                          .resizable()
-                          .aspectRatio(contentMode: .fit)
-                          .frame(width:80)
-                          .padding()
-                      Text("Nutrition")
-                      }}
-                NavigationLink(destination: MedicineView())
-                  {HStack{
-                      Image("drugs")
-                          .resizable()
-                          .aspectRatio(contentMode: .fit)
-                          .frame(width:80)
-                          .padding()
-                      Text("Medications")
-                      }}
-                NavigationLink(destination: AdvancedCareView())
-                  {HStack{
-                      Image("plan")
-                          .resizable()
-                          .aspectRatio(contentMode: .fit)
-                          .frame(width:80)
-                          .padding()
-                      Text("Advance Care Planning")
-                      }}
-//                NavigationLink(destination: FrailtyView())
-//                  {HStack{
-//                      Image("arm")
-//                          .resizable()
-//                          .aspectRatio(contentMode: .fit)
-//                          .frame(width:80)
-//                          .padding()
-//                      Text("Frailty")
-//                      }}
-                // TODO: Seems max number of views allowed is 10? Need more research to find root issue and solution
-                HStack{
-//                        Image("survey")
-//                        .resizable()
-//                        .aspectRatio(contentMode: .fit)
-//                        .frame(width:80)
-//                        .padding()
-                        Text("Click here to take a survey about GeriKit.")
-                        .foregroundColor(.blue)
-                        .onTapGesture {
-                            let url = URL.init(string: "https://nyumc.qualtrics.com/jfe/form/SV_9NrUEStP5yiGbXL")
-                            guard let survey = url, UIApplication.shared.canOpenURL(survey) else { return }
-                            UIApplication.shared.open(survey)}
-                    
+                    NavigationLink(destination: ADLView())
+                    {HStack{
+                        Image("elderly")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width:80)
+                            .padding()
+                        Text("Function")
+                    }}
+                    NavigationLink(destination: ChairView())
+                    {HStack{
+                        Image("chair")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width:80)
+                            .padding()
+                        Text("Strength")
+                    }}
+                }
+                Group {
+                    NavigationLink(destination: FallView())
+                    {HStack{
+                        Image("fall")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width:80)
+                            .padding()
+                        Text("Falls")
+                    }}
+                    NavigationLink(destination: NutritionView())
+                    {HStack{
+                        Image("kitchen")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width:80)
+                            .padding()
+                        Text("Nutrition")
+                    }}
+                    NavigationLink(destination: MedicineView())
+                    {HStack{
+                        Image("drugs")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width:80)
+                            .padding()
+                        Text("Medications")
+                    }}
+                    NavigationLink(destination: AdvancedCareView())
+                    {HStack{
+                        Image("plan")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width:80)
+                            .padding()
+                        Text("Advance Care Planning")
                     }
-                HStack{
-                        Text("© NYU Grossman School of Medicine 2020")
-                          .frame(maxWidth: .infinity, alignment: .center)
-                        // .foregroundColor(.blue)
-                        // .onTapGesture {
-                        //     let url = URL.init(string: "https://unc.az1.qualtrics.com/jfe/form/SV_0PScJUobnquCRa5")
-                        //     guard let survey = url, UIApplication.shared.canOpenURL(survey) else { return }
-                        //     UIApplication.shared.open(survey)}
-                    
                     }
-            }.navigationBarTitle("GeriKit", displayMode: .automatic)
-            .navigationBarItems(trailing:
-                HStack {
-                    NavigationLink(destination:AboutView()){Text("About")}
-
-                })
+                    Group {
+                        NavigationLink(destination: FrailtyView())
+                        {HStack{
+                            Image("arm")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width:80)
+                                .padding()
+                            Text("Frailty")
+                        }}
+                    }
+                    Group {
+                        HStack{
+                            //                        Image("survey")
+                            //                        .resizable()
+                            //                        .aspectRatio(contentMode: .fit)
+                            //                        .frame(width:80)
+                            //                        .padding()
+                            Text("Click here to take a survey about GeriKit.")
+                                .foregroundColor(.blue)
+                                .onTapGesture {
+                                    let url = URL.init(string: "https://nyumc.qualtrics.com/jfe/form/SV_9NrUEStP5yiGbXL")
+                                    guard let survey = url, UIApplication.shared.canOpenURL(survey) else { return }
+                                    UIApplication.shared.open(survey)}
+                            
+                        }
+                        HStack{
+                            Text("© NYU Grossman School of Medicine 2023")
+                                .frame(maxWidth: .infinity, alignment: .center)
+                            // .foregroundColor(.blue)
+                            // .onTapGesture {
+                            //     let url = URL.init(string: "https://unc.az1.qualtrics.com/jfe/form/SV_0PScJUobnquCRa5")
+                            //     guard let survey = url, UIApplication.shared.canOpenURL(survey) else { return }
+                            //     UIApplication.shared.open(survey)}
+                            
+                        }
+                    }.navigationBarTitle("GeriKit", displayMode: .automatic)
+                        .navigationBarItems(trailing:
+                                                HStack {
+                            NavigationLink(destination:AboutView()){Text("About")}
+                            
+                        })
+                    
+                }}
             }
         }
     }
--- SurveyApp/FrailtyView.swift
+++ SurveyApp/FrailtyView.swift
@@ -2,15 +2,50 @@
 //  FrailtyView.swift
 //  SurveyApp
 //
-//  Created by Gabriela Lopez on 1/23/23.
-//  Copyright © 2023 Ambika Viswanathan. All rights reserved.
+//  Created by Ambika Viswanathan on 10/3/20.
+//  Copyright © 2020 Ambika Viswanathan. All rights reserved.
 //
 
 import SwiftUI
 
 struct FrailtyView: View {
+    @State var Frailty = Bundle.main.decode("Frailty.json")
+    @State var FrailtyScore: Array<Int> = Array(repeating: 0, count: 100)
+    @State var FrailtySum = 0
+    @State var weightNow: Int = 100
+    @State var weightYear: Int = 100
+
     var body: some View {
-        // TODO: Build in interactive frail scale
-        Text("Placeholder")
+            List{
+                Section(header: Text("Ask the participant the following questions..")) {
+                    QuestionView(fetcher: $Frailty, scores: $FrailtyScore, sum: $FrailtySum)
+                }
+                
+                Section(header: Text("One year ago, how much did you weigh? (lbs)")) {
+                        TextField("", value: $weightYear, formatter: NumberFormatter())
+                            .frame(width: 50)
+
+                }
+                
+                Section(header: Text("How much do you weigh currently? (lbs)")) {
+                    TextField("", value: $weightNow, formatter: NumberFormatter())
+                        .frame(width: 50)
+                    
+                }
+                
+                Section {
+                    NavigationLink(destination: FrailtyResults(frailty_sum: $FrailtySum, cur_weight: $weightNow, old_weight: $weightYear))
+                            {Text("Click here for Frailty result:").bold()
+                            }.foregroundColor(.blue)
+                    }
+            }
+            .navigationBarTitle("Frailty")
+        
+    }
+}
+
+struct FrailtyView_Previews: PreviewProvider {
+    static var previews: some View {
+        FrailtyView()
     }
 }
